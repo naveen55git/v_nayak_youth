6dd99933977fc422b0e7943a28891a6149f746ec
V Nayak Youth <mrwhitedevil2003@gmail.com>
2026-09-13 19:20:00 +0530

    Optimize deployment build scripts, render config, and production media routing

--- vnayak_portal/urls.py
+++ vnayak_portal/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('', include('youth_portal.urls')),
 ]
 
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
